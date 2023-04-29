a405062142747c3c43f661e16042d6d0666ed7d3
Dracuila <mail.mailyan@list.ru>
2023-04-30 01:27:37 +0400

    first commit

---
+++ dist/js/demo.js
@@ -0,0 +1,13 @@
+/**
+ * AdminLTE Demo Menu
+ * ------------------
+ * You should not use this file in production.
+ * This file is for demo purposes only.
+ */
+(function($) {
+
+
+    $(".a").css({ "background-color": "rgba(255,255,255,.9)", "color": "black" });
+
+
+})(jQuery)
